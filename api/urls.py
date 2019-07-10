46fc448c36c000b51cf056d369e224c9c18138f7
creedowl <creedowl@gmail.com>
2019-07-10 18:01:14 +0800

    add channels to manager websocket

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls import url
 from django.urls import path, include
+
+from api import gdbWsConsumer
 from . import gdbContr
 
 urlpatterns = [
@@ -8,4 +11,9 @@ urlpatterns = [
     path('disassemble', gdbContr.disassemble),
     path('break', gdbContr.break_gdb),
     path('next', gdbContr.next_gdb),
+]
+
+websocket_urlpatterns = [
+    # url(r'^ws/gdb/(?P<client_id>[^/]+)/$', gdbWsConsumer.Consumer),
+    path('ws/gdb/<client_id>', gdbWsConsumer.Consumer)
 ]
